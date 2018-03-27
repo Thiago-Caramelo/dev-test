7ae07b6c580e28a0677f23858a4a25f1c0456f84
Thiago Caramelo <thiago.pescado@gmail.com>
2018-03-27 00:20:20 -0300

    initial commit

---
+++ src/Services/OrderService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Services
+{
+    public class OrderService
+    {
+        public void OrderBurger() {
+            throw new NotImplementedException();
+        }
+    }
+}
