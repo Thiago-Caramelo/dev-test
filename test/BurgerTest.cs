ae7b5f365fe239d0cd22e3203631c103ed74a143
Thiago Caramelo <thiago.pescado@gmail.com>
2018-03-30 18:57:52 -0300

    test(burger) : repository for each test

--- test/BurgerTest.cs
+++ test/BurgerTest.cs
@@ -6,14 +6,15 @@ namespace Test
     [TestClass]
     public class BurgerTest
     {
-        private readonly TestRepository _repository = new TestRepository();
-
         [TestMethod]
         public void XBacon_Price_Should_Be_6_50()
-        {
-            var burger = _repository.GetBurguerByType(BurgerType.XBacon);
+        {   
+            const decimal xBaconPrice = 6.50m;
+
+            var repository = new TestRepository();
+            var burger = repository.GetBurguerByType(BurgerType.XBacon);
             var burgerPrice = burger.Price();
-            Assert.AreEqual<decimal>(6.50m, burgerPrice);
+            Assert.AreEqual<decimal>(xBaconPrice, burgerPrice);
             // pegar a quantidade pedida e ver se é multiplo de 3, ou seja se dividir por 3 e não tive resto
             // para saber quanto o cliente vai pagar é só dividir por 1,5
         }
