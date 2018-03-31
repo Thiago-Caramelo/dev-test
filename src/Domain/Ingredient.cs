3f2711553ebd88d653f1d2b39735067dc3c53eb4
Thiago Caramelo <thiago.pescado@gmail.com>
2018-03-31 00:26:08 -0300

    feat(ingredient) : ingredient type

--- src/Domain/Ingredient.cs
+++ src/Domain/Ingredient.cs
@@ -6,6 +6,7 @@ namespace Domain
     {
         public int IngredientID { get; set; }
         public string Description { get; set; }
-        public decimal Price { get; set; }   
+        public decimal Price { get; set; }
+        public IngredientType IngredientType { get; set; }
     }
 }
---
+++ src/Domain/IngredientType.cs
@@ -0,0 +1,11 @@
+namespace Domain
+{
+    public enum IngredientType
+    {
+        Lettuce = 1,
+        Bacon = 2,
+        Hamburger = 3,
+        Egg = 4,
+        Cheese = 5
+    }
+}
