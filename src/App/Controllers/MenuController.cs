963f6ee778c5f1e4401e697ff5109834bbcd7cd4
Thiago Caramelo <thiago.pescado@gmail.com>
2018-04-01 12:24:20 -0300

    feat(controller) : menu controller di

--- src/App/Controllers/MenuController.cs
+++ src/App/Controllers/MenuController.cs
@@ -5,13 +5,24 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using app.Models;
+using Infra;
+using Services;
 
 namespace app.Controllers
 {
     public class MenuController : Controller
     {
+        private readonly IRepository _repository;
+        private readonly MenuService _menuService;
+        public MenuController(IRepository repository, MenuService menuService)
+        {
+            _repository = repository;
+            _menuService = menuService;
+        }
         public IActionResult Index()
         {
+            var burgers = _repository.GetMenuBurgers();
+
             return View();
         }
     }
